5016164f056fd03456e992e41961d20fd180c196
b0pez <blopez4434@gmail.com>
2022-11-17 10:03:25 -0300

    mensaje de update listo

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -2,6 +2,8 @@ import { useEffect, useMemo } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { SaveOutlined } from '@mui/icons-material';
 import { Button, Grid, TextField, Typography } from '@mui/material';
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 import { useForm } from '../../hooks/useForm';
 import { ImageGallery } from '../components';
@@ -9,7 +11,7 @@ import { setActiveNote, startSaveNote } from '../../store/journal';
 
 export const NoteView = () => {
 
-  const { active:note } = useSelector(state => state.journal);
+  const { active:note, messageSaved, isSaving } = useSelector(state => state.journal);
   const dispatch = useDispatch();
 
   const { body, title,  date, onInputChange, formState } = useForm(note);
@@ -18,6 +20,13 @@ export const NoteView = () => {
     const newDate = new Date(date);
     return newDate.toUTCString();
   }, [date]);
+
+  useEffect(() => {
+    if( messageSaved.length > 0 ) {
+      Swal.fire('Nota actualizada', messageSaved, 'success');
+    }
+  }, [messageSaved]);
+  
 
   useEffect(() => {
     dispatch( setActiveNote(formState) );
@@ -41,7 +50,8 @@ export const NoteView = () => {
         </Typography>
       </Grid>
       <Grid item>
-        <Button 
+        <Button
+          disabled={ isSaving } 
           color="primary" 
           sx={{ padding: 2 }}
           onClick={ onSaveNote }
